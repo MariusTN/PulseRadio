da26c2471013ae576b227c197fc81fea8d4ee085
MariusTN <pulsemusic8@gmail.com>
2020-01-21 17:46:05 +0000

    CSS UPDATE - Bootstrap Removed

--- js.js
+++ js.js
@@ -17,25 +17,25 @@ let pastSong0 = document.getElementsByClassName('ps0')[0]
 async function fetchStarship() {
 	let response = await fetch('https://myradio24.com/users/78810/status.json')
 	let data = await response.json();
-	titluArtist.innerHTML=`Artist Name: ${data.artist}`;
-	titluMelodie.innerHTML=`Song Name: ${data.songtitle}`;
-	nextSong.innerHTML=`Next Song: ${data.nextsongs[0]}`;
-	pastSong0.innerHTML = `${data.songs[9][0]} : ${data.songs[9][1]}`;
-	pastSong1.innerHTML = `${data.songs[0][0]} : ${data.songs[0][1]}`;
-	pastSong2.innerHTML = `${data.songs[1][0]} : ${data.songs[1][1]}`;
-	pastSong3.innerHTML = `${data.songs[2][0]} : ${data.songs[2][1]}`;
-	pastSong4.innerHTML = `${data.songs[3][0]} : ${data.songs[3][1]}`;
-	pastSong5.innerHTML = `${data.songs[4][0]} : ${data.songs[4][1]}`;
-	pastSong6.innerHTML = `${data.songs[5][0]} : ${data.songs[5][1]}`;
-	pastSong7.innerHTML = `${data.songs[6][0]} : ${data.songs[6][1]}`;
-	pastSong8.innerHTML = `${data.songs[7][0]} : ${data.songs[7][1]}`;
-	pastSong9.innerHTML = `${data.songs[8][0]} : ${data.songs[8][1]}`;
+	titluArtist.innerHTML = `Artist Name: ${data.artist}`;
+	titluMelodie.innerHTML = `Song Name: ${data.songtitle}`;
+	nextSong.innerHTML = `Next Song: ${data.nextsongs[0]}`;
+	pastSong0.innerHTML = `1. ${data.songs[9][1]}`;
+	pastSong1.innerHTML = `10. ${data.songs[0][1]}`;
+	pastSong2.innerHTML = `9. ${data.songs[1][1]}`;
+	pastSong3.innerHTML = `8. ${data.songs[2][1]}`;
+	pastSong4.innerHTML = `7. ${data.songs[3][1]}`;
+	pastSong5.innerHTML = `6. ${data.songs[4][1]}`;
+	pastSong6.innerHTML = `5. ${data.songs[5][1]}`;
+	pastSong7.innerHTML = `4. ${data.songs[6][1]}`;
+	pastSong8.innerHTML = `3. ${data.songs[7][1]}`;
+	pastSong9.innerHTML = `2. ${data.songs[8][1]}`;
 }
 
-setInterval(fetchStarship, 5000); 
+setInterval(fetchStarship, 5000);
 
 //custom Particles
 
-particlesJS.load('particles-js', './particlesjs-config.json', function() {
+particlesJS.load('particles-js', './particlesjs-config.json', function () {
 	console.log('Particles Loaded');
-  });
+});
