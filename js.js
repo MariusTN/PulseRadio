3f9fe76054edd1df41d4a4ac20565008517255df
MariusTN <pulsemusic8@gmail.com>
2020-10-25 19:36:29 +0000

    Update Cleanup code - responsive

--- js.js
+++ js.js
@@ -1,25 +1,23 @@
-let titluMelodie = document.getElementsByClassName('melodie')[0];
-let titluArtist = document.getElementsByClassName('artist')[0];
-let titluMelodie2 = document.getElementsByClassName('melodie1')[0];
-let titluArtist2 = document.getElementsByClassName('artist1')[0];
-let nextSong = document.getElementsByClassName('nsong')[0];
-let pastSong1 = document.getElementsByClassName('ps1')[0]
-let pastSong2 = document.getElementsByClassName('ps2')[0]
-let pastSong3 = document.getElementsByClassName('ps3')[0]
-let pastSong4 = document.getElementsByClassName('ps4')[0]
-let pastSong5 = document.getElementsByClassName('ps5')[0]
-let pastSong6 = document.getElementsByClassName('ps6')[0]
-let pastSong7 = document.getElementsByClassName('ps7')[0]
-let pastSong8 = document.getElementsByClassName('ps8')[0]
-let pastSong9 = document.getElementsByClassName('ps9')[0]
-let pastSong0 = document.getElementsByClassName('ps0')[0]
+let titluMelodie = document.querySelector('.melodie');
+let titluArtist = document.querySelector('.artist');
+let titluMelodie2 = document.querySelector('.melodie1');
+let titluArtist2 = document.querySelector('.artist1');
+let pastSong1 = document.querySelector('.ps1');
+let pastSong2 = document.querySelector('.ps2');
+let pastSong3 = document.querySelector('.ps3');
+let pastSong4 = document.querySelector('.ps4');
+let pastSong5 = document.querySelector('.ps5');
+let pastSong6 = document.querySelector('.ps6');
+let pastSong7 = document.querySelector('.ps7');
+let pastSong8 = document.querySelector('.ps8');
+let pastSong9 = document.querySelector('.ps9');
+let pastSong0 = document.querySelector('.ps0');
 
 async function fetchStarship() {
-	let response = await fetch('https://myradio24.com/users/78810/status.json')
+	let response = await fetch('https://myradio24.com/users/2288/status.json');
 	let data = await response.json();
 	titluArtist.innerHTML = `Artist Name: ${data.artist}`;
 	titluMelodie.innerHTML = `Song Name: ${data.songtitle}`;
-	nextSong.innerHTML = `Next Song: ${data.nextsongs[0]}`;
 	pastSong0.innerHTML = `1. ${data.songs[9][1]}`;
 	pastSong1.innerHTML = `10. ${data.songs[0][1]}`;
 	pastSong2.innerHTML = `9. ${data.songs[1][1]}`;
@@ -32,10 +30,9 @@ async function fetchStarship() {
 	pastSong9.innerHTML = `2. ${data.songs[8][1]}`;
 }
 
-setInterval(fetchStarship, 5000);
+setInterval(fetchStarship, 6000);
 
 //custom Particles
 
 particlesJS.load('particles-js', './particlesjs-config.json', function () {
-	console.log('Particles Loaded');
 });
